531e2d325f2688e3d6498cdcae87c19cff6954e4
onulee <onulee@naver.com>
2025-07-24 17:05:06 +0900

    1차원배열, 2차원배열 변환

---
+++ Ja0724/src/C0724_13.java
@@ -0,0 +1,13 @@
+
+public class C0724_13 {
+
+	public static void main(String[] args) {
+		int[] num = new int[25];
+		String[][] arr = new String[5][5];
+		
+		//1-25까지 숫자 입력, 랜덤으로 번호섞어서 arr배열에 입력해서 출력하시오.
+		// num배열 - int, arr배열 - String
+
+	}
+
+}
