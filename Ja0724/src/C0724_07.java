70865f8781f8936bf3140af573718521c59fef36
onulee <onulee@naver.com>
2025-07-24 15:13:03 +0900

    학생성적프로그램-입력,출력,수정,종료

---
+++ Ja0724/src/C0724_07.java
@@ -0,0 +1,16 @@
+
+public class C0724_07 {
+
+	public static void main(String[] args) {
+		// 이름,국어,영어,수학 점수를 입력받아 출력하시오.
+		// 1.변수배열선언 - name,score,total,avg
+		// 2. 값입력 - 이름,
+		// 3. score 값입력 - 국어,영어,수학
+		// 4. total,avg 값입력
+		// 5. 출력
+		
+		
+
+	}
+
+}
---
+++ Ja0724/src/C0724_10.java
@@ -0,0 +1,14 @@
+
+public class C0724_10 {
+
+	public static void main(String[] args) {
+		// 1.변수배열선언
+		// 2.출력구문 - while(true), switch구문
+		// 3.입력부분
+		// 4.취소부분추가
+		// 5.출력부분
+		// 6.프로그램종료부분
+
+	}
+
+}
