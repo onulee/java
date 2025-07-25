64d3c7b59eafcf833c5906369bbf60be9724307e
onulee <onulee@naver.com>
2025-07-25 12:55:13 +0900

    배열 정리

--- Ja0725/src/C0725_06.java
+++ Ja0725/src/C0725_06.java
@@ -1,9 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class C0725_06 {
 
 	public static void main(String[] args) {
 //		// 배열
-		int[] arr = new int[45];
+		Scanner scan = new Scanner(System.in);
+		// 
+		int[] arr = new int[45];  //랜덤번호 45개
+		int[] lotto = new int[6]; //로또번호 6개
+		int[] myNo = new int[6];  //입력번호 6개
+		int[] okNo = new int[6];  //정답번호 6개
+		int count = 0;            //정답개수
+		
+		
+		for(int i=0;i<6;i++) {
+			System.out.println("입력된 번호 : "+Arrays.toString(myNo));
+			System.out.println("1-45까지 번호를 입력하세요.>> ");
+			myNo[i] = scan.nextInt();
+		}
+		
+		
 		//배열 번호입력
 		for(int i=0;i<45;i++) {
 			arr[i] = i+1;
@@ -17,12 +34,17 @@ public class C0725_06 {
 			temp = arr[0];
 			arr[0] = arr[rnum];
 			arr[rnum] = temp;
-			
 		}
 		
-		//배열 번호출력
+		// 배열복사 메소드사용
+		System.arraycopy(arr, 0, lotto, 0, 6);
+		
+		
+		
+		// 로또번호출력
+		System.out.print("로또번호 : ");
 		for(int i=0;i<6;i++) {
-			System.out.print(arr[i]+" ");
+			System.out.print(lotto[i]+" ");
 		}
 		
 		
