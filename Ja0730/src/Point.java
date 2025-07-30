bc68a5724de4b21398eafa4a0fe91d25742526b8
onulee <onulee@naver.com>
2025-07-30 10:20:39 +0900

    상속

---
+++ Ja0730/src/Point.java
@@ -0,0 +1,18 @@
+
+public class Point {
+	int x; //인스턴스 변수 - 객체선언후,개별,참조변수명.변수명
+	int y;
+	
+	Point(){
+		this(0,0); // this() : 다른생성자
+	}
+	Point(int x,int y){
+		this.x = x; //this : 인스턴스변수
+		this.y = y;
+	}
+	
+	
+	void add() {
+		System.out.println(x+y);
+	}
+}
---
+++ Ja0730/src/Shape.java
@@ -0,0 +1,9 @@
+
+public class Shape {
+	String color = "blue";
+	void draw(String sname) {
+		System.out.println(sname + " 도형의 그림을 그립니다.");
+		// 하단으로 그림을 그리는 로직이 더 들어가야 함.
+		// ...
+	}
+}
