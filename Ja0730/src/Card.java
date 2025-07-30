e10ff9916368cd58b26f9bed8026af73695636a5
onulee <onulee@naver.com>
2025-07-30 12:20:30 +0900

    카드

--- Ja0730/src/Card.java
+++ Ja0730/src/Card.java
@@ -3,10 +3,19 @@ public class Card {
 	int number;
 	String kind;
 	
+	String[] c_shape = {
+			"","A","2","3","4","5","6","7","8","9","10","J","Q","K"	
+		};
+	
 	Card(){}
 	Card(int number,String kind){
 		this.number= number;
 		this.kind = kind;
+	}
+	
+	@Override
+	public String toString() {
+		return "[ "+c_shape[number]+","+kind+" ]";
 	}
 
 }
