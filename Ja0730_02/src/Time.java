9c16be72cd7f3abc523ec7cd5eb02a921b29b9e2
onulee <onulee@naver.com>
2025-07-30 15:26:14 +0900

    상속

---
+++ Ja0730_02/src/Time.java
@@ -0,0 +1,18 @@
+
+public class Time {
+	private int hour;
+	private int minute;
+	private int second;
+	
+	Time(){}
+	Time(int hour,int minute,int second){
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+	}
+	
+	//getter,setter를 생성
+	
+	
+	
+}
