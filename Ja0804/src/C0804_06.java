c896421285face83d99037ece060fd7df6a47911
user <user@KOSMO>
2025-08-04 18:22:27 +0900

    파일 입출력

--- Ja0804/src/C0804_06.java
+++ Ja0804/src/C0804_06.java
@@ -3,13 +3,21 @@ import java.util.ArrayList;
 public class C0804_06 {
 
 	public static void main(String[] args) {
-		ArrayList list = new ArrayList();
+		ArrayList<Stu> list = new ArrayList(); //제너릭스
 		
 		list.add(new Stu("홍길동",100,100,100));
 		list.add(new Stu("유관순",90,90,100));
 		list.add(new Stu("이순신",80,100,100));
+//		list.add(new St(1,"이순신","010-0000-0000")); //에러가 안남.
 		list.add(new Stu("강감찬",70,100,100));
 		list.add(new Stu("김구",60,100,100));
+		
+		for(int i=0;i<list.size();i++) {
+//			Stu s = (Stu)list.get(i); //형변환
+//			System.out.println(s.getName()+","+s.getKor());
+			System.out.println(list.get(i).getName()+","
+               +list.get(i).getKor());
+		}
 		
 		
 
--- Ja0804/src/St.java
+++ Ja0804/src/St.java
@@ -4,4 +4,10 @@ public class St {
 	private int number;
 	private String name;
 	private String phone;
+	
+	St(int number,String name,String phone){
+		this.number = number;
+		this.name = name;
+		this.phone = phone;
+	}
 }
