7511ffd6559f29332f48c349cd1fd7b6402402e9
onulee <onulee@naver.com>
2025-08-05 16:26:09 +0900

    파일저장,쓰레드

---
+++ Ja0805/src/C0805_main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class C0805_main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		//파일읽기 - 4명의 학생성적을 객체에 담아서 출력
+		
+		
+		while(true) {
+			System.out.println("[ 학생성적 프로그램 ]");
+			System.out.println("1. 성적입력");
+			System.out.println("2. 성적출력");
+			System.out.println("------------");
+			System.out.println("원하는 번호를 입력하세요.>> ");
+			int choice = scan.nextInt();
+			
+			switch(choice) {
+			case 1:
+				
+				break;
+			}
+		}
+
+	}
+
+}
---
+++ Ja0805/src/MyThread2.java
@@ -0,0 +1,9 @@
+
+public class MyThread2 extends Thread {
+	public void run() {
+		for(int i=0;i<300;i++) {
+			System.out.println(i+": ||||");
+		}
+	}
+
+}
